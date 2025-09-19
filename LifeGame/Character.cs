2d6ab9801fa3abb69e9e88a0b9b3380a47a80cd3
Haruo <haruohwr@gmail.com>
2025-09-18 20:29:27 -0500

    Character
    Abstract class

---
+++ LifeGame/Character.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame
+{
+    abstract class Character
+    {
+        protected int Energy { get; set; }
+        protected Character(int startingEnergy)
+        {
+            Energy = startingEnergy;
+        }
+
+        public abstract void Act(Simulation sim);
+    }
+}
