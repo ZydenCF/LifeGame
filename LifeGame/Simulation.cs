f6016f76752aac41acb5c05b463eb9ee99af6e4b
David Ccolcca <draigner143@gmail.com>
2025-09-22 17:42:53 -0500

    Simulation

---
+++ LifeGame/Simulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeGame
+{
+    class Simulation
+    {
+        public int Resources { get; set; }
+
+        public Simulation()
+        {
+            Resources = 50;
+        }
+    }
+}
